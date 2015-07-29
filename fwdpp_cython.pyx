a54e54c2f85651cadf243f44b415c90d5e1e16cb
Kevin R. Thornton <krthornt@uci.edu>
2015-07-29 16:27:44 -0700

    pyx file added

---
+++ fwdpp_cython.pyx
@@ -0,0 +1,24 @@
+# distutils: language = c++
+# distutils: sources = neutral.cpp
+
+from libcpp.vector cimport vector
+
+##Create hooks to C++ types
+
+#This is the "singlepop" type from fwdpp/sugar/singlepop.hpp
+cdef extern from "neutral.hpp" namespace "fwdpp_cython":
+    cdef cppclass singlepop_t:
+        singlepop_t(unsigned,unsigned)
+
+cdef extern from "neutral.hpp" namespace "fwdpp_cython":
+    cdef vector[int] sfs(const unsigned & seed,const singlepop_t & pop,const unsigned & nsam)
+    
+cdef class PySinglepop:
+    cdef singlepop_t *thisptr
+    def __cinit__(self,int N, int reserve_size = 100):
+        self.thisptr = new singlepop_t(N,reserve_size)
+    def __dealloc__(self):
+        del self.thisptr
+
+
+
